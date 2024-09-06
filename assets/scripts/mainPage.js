a5809ff3c91b081ffe5f6e40f70f787e31ff74fa
Gerasimchuk Vlad <gerasimcukvlad37@gmail.com>
2024-09-06 12:57:28 +0300

    fix

--- assets/scripts/mainPage.js
+++ assets/scripts/mainPage.js
@@ -3,13 +3,16 @@ if (navButton) {
 	let mapBlock = document.querySelector(".schools-info__map");
 	let listBlock = document.querySelector(".schools-info__list");
 	navButton.addEventListener("click", function () {
+		console.log("1");
 		if (navButton.classList.contains("map")) {
+			console.log(2);
 			navButton.classList.remove("map");
 			navButton.classList.add("list");
 			mapBlock.classList.add("_hidden");
 			listBlock.classList.remove("_hidden");
 			navButton.innerHTML = `<img src="./assets/imgs/map-icon.png" alt="" /><span>Map</span>`;
-		} else {
+		} else if (navButton.classList.contains("list")) {
+			console.log(3);
 			navButton.classList.add("map");
 			navButton.classList.remove("list");
 			mapBlock.classList.remove("_hidden");
