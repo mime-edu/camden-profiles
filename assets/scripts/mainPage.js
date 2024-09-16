8a1646ec08c55f896c53efee19dce6b6211c0331
Gerasimchuk Vlad <gerasimcukvlad37@gmail.com>
2024-09-17 00:09:47 +0300

    commit vue.js functions & part of schools json

--- assets/scripts/mainPage.js
+++ assets/scripts/mainPage.js
@@ -1,73 +1,84 @@
 import { createTab } from "./modules/tabs.js";
+const { createApp, ref } = Vue;
+// let schools = ref([{ title: "School" }, { title: "School1" }, { title: "School2" }, { title: "School3" }]);
+document.addEventListener("DOMContentLoaded", async function () {
+	const response = await fetch("./schools.json");
+	let schools = ref(await response.json());
 
-let mainPageTabItem = document.querySelector(".hero__tab");
-if (mainPageTabItem) {
-	createTab(mainPageTabItem);
-}
-// main page navigation button (change list to map)
-let navButton = document.querySelector(".navigation__button");
-if (navButton) {
-	let mapBlock = document.querySelector(".schools-info__map");
-	let listBlock = document.querySelector(".schools-info__list");
-	navButton.addEventListener("click", function () {
-		console.log("1");
-		if (navButton.classList.contains("map")) {
-			console.log(2);
-			navButton.classList.remove("map");
-			navButton.classList.add("list");
-			mapBlock.classList.add("_hidden");
-			listBlock.classList.remove("_hidden");
-			navButton.innerHTML = `<img src="./assets/imgs/map-icon.png" alt="" /><span>Map</span>`;
-		} else if (navButton.classList.contains("list")) {
-			console.log(3);
-			navButton.classList.add("map");
-			navButton.classList.remove("list");
-			mapBlock.classList.remove("_hidden");
-			listBlock.classList.add("_hidden");
-			navButton.innerHTML = `<img src="./assets/imgs/list-icon.png" alt="" /><span>List</span>    `;
+	// Vue.js app initialization
+	createApp({
+		setup() {
+			return { schools };
+		},
+	}).mount("#mainPage");
+
+	// Tab item setup
+	let mainPageTabItem = document.querySelector(".hero__tab");
+	if (mainPageTabItem) {
+		createTab(mainPageTabItem);
+	}
+
+	// Main page navigation button (change list to map)
+	let navButton = document.querySelector(".navigation__button");
+	if (navButton) {
+		let mapBlock = document.querySelector(".schools-info__map");
+		let listBlock = document.querySelector(".schools-info__list");
+		navButton.addEventListener("click", function () {
+			if (navButton.classList.contains("map")) {
+				navButton.classList.remove("map");
+				navButton.classList.add("list");
+				mapBlock.classList.add("_hidden");
+				listBlock.classList.remove("_hidden");
+				navButton.innerHTML = `<img src="./assets/imgs/map-icon.png" alt="" /><span>Map</span>`;
+			} else if (navButton.classList.contains("list")) {
+				navButton.classList.add("map");
+				navButton.classList.remove("list");
+				mapBlock.classList.remove("_hidden");
+				listBlock.classList.add("_hidden");
+				navButton.innerHTML = `<img src="./assets/imgs/list-icon.png" alt="" /><span>List</span>`;
+			}
+		});
+	}
+
+	// Filter overlay setup
+	let openFilterOverlayBtn = document.querySelector(".schools-filter__button");
+	if (openFilterOverlayBtn) {
+		openFilterOverlayBtn.addEventListener("click", openFilterOverlay);
+	}
+
+	let filterOverlay = document.querySelector(".filter-overlay");
+	if (filterOverlay) {
+		let closeButton = filterOverlay.querySelector(".filter-overlay__close");
+		if (closeButton) {
+			closeButton.addEventListener("click", closeFilterOverlay);
 		}
-	});
-}
-
-// close/open filter overlay
-let openFilterOverlayBtn = document.querySelector(".schools-filter__button");
-if (openFilterOverlayBtn) {
-	openFilterOverlayBtn.addEventListener("click", openFilterOverlay);
-}
-let filterOverlay = document.querySelector(".filter-overlay");
-if (filterOverlay) {
-	let closeButton = filterOverlay.querySelector(".filter-overlay__close");
-	if (closeButton) {
-		closeButton.addEventListener("click", closeFilterOverlay);
-	}
-}
-
-// close filter overlay
-function closeFilterOverlay() {
-	let filterOverlay = document.querySelector(".filter-overlay");
-	if (!filterOverlay) {
-		return;
-	}
-	filterOverlay.classList.remove("_active");
-	filterOverlay.classList.add("_closed");
-}
-// open filter overlay
-function openFilterOverlay() {
-	let filterOverlay = document.querySelector(".filter-overlay");
-	if (!filterOverlay) {
-		return;
 	}
 
-	filterOverlay.classList.remove("_closed");
-	filterOverlay.classList.add("_active");
-}
+	// Close filter overlay function
+	function closeFilterOverlay() {
+		let filterOverlay = document.querySelector(".filter-overlay");
+		if (!filterOverlay) {
+			return;
+		}
+		filterOverlay.classList.remove("_active");
+		filterOverlay.classList.add("_closed");
+	}
 
-// overlay age range
-let ageRangeInput = document.querySelector(".age-range-input");
-console.log(ageRangeInput);
-if (ageRangeInput) {
-	ageRangeInput.addEventListener("input", function () {
-		ageRangeInput = document.querySelector(".age-range-input");
-		document.querySelector(".age-range").querySelector(".minValue").innerText = ageRangeInput.value;
-	});
-}
+	// Open filter overlay function
+	function openFilterOverlay() {
+		let filterOverlay = document.querySelector(".filter-overlay");
+		if (!filterOverlay) {
+			return;
+		}
+		filterOverlay.classList.remove("_closed");
+		filterOverlay.classList.add("_active");
+	}
+
+	// Age range input handling
+	let ageRangeInput = document.querySelector(".age-range-input");
+	if (ageRangeInput) {
+		ageRangeInput.addEventListener("input", function () {
+			document.querySelector(".age-range .minValue").innerText = ageRangeInput.value;
+		});
+	}
+});
--- assets/scripts/schoolContext.js
+++ assets/scripts/schoolContext.js
@@ -1,35 +1,51 @@
-const checkbox = document.querySelector("#switch2");
-const comparisonsItems = document.querySelectorAll(".stats-sector__comparisons");
+const { createApp, ref } = Vue;
 
-checkbox.addEventListener("change", function () {
-	if (checkbox.checked) {
-		for (const item of comparisonsItems) {
-			item.classList.remove("_hidden");
+document.addEventListener("DOMContentLoaded", async function () {
+	const response = await fetch("../../schools.json");
+	let schools = await response.json();
+	let schoolId = 1;
+	let school = ref(
+		schools.find((el) => {
+			return el.schoolId == schoolId;
+		})
+	);
+	let currentInfoBlock = ref(1);
+	createApp({
+		setup() {
+			return { school, currentInfoBlock: currentInfoBlock };
+		},
+	}).mount("#schoolContext");
+
+	// page functions
+
+	const checkbox = document.querySelector("#switch2");
+	const comparisonsItems = document.querySelectorAll(".stats-sector__comparisons");
+
+	checkbox.addEventListener("change", function () {
+		if (checkbox.checked) {
+			for (const item of comparisonsItems) {
+				item.classList.remove("_hidden");
+			}
+		} else {
+			for (const item of comparisonsItems) {
+				item.classList.add("_hidden");
+			}
 		}
-	} else {
-		for (const item of comparisonsItems) {
-			item.classList.add("_hidden");
-		}
+	});
+
+	// statistic change
+
+	let statNavigation = document.querySelector(".statistics-nav");
+	if (statNavigation) {
+		let buttons = document.querySelectorAll(".statistics-nav__button");
+		buttons.forEach((button) => {
+			button.addEventListener("click", function () {
+				for (const button of buttons) {
+					button.classList.remove("active");
+				}
+				button.classList.add("active");
+				currentInfoBlock.value = +button.getAttribute("currentBlock");
+			});
+		});
 	}
 });
-
-// statistic change
-
-let statNavigation = document.querySelector(".statistics-nav");
-if (statNavigation) {
-	let buttons = document.querySelectorAll(".statistics-nav__button");
-	buttons.forEach((button) => {
-		button.addEventListener("click", function () {
-			for (const button of buttons) {
-				button.classList.remove("active");
-			}
-			button.classList.add("active");
-			// openStatisticsPage();
-		});
-	});
-}
-
-function openStatisticsPage() {
-	let statisticsBlockWrapper = document.querySelector(".statistics-block");
-	statisticsBlockWrapper.innerHTML = ``;
-}
